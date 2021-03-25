d64f7373ac9d3198f220494c580159e25adfdb94
Ananya <72216605+ananyachibber21@users.noreply.github.com>
2021-03-25 08:07:32 +0530

    Create matrix3.cpp

---
+++ matrix3.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main()
+
+{
+	int n;
+	scanf("%d",&n);
+	for(int i=1;i<=n;i++)
+	{
+		for(int k=5-i;k>=1;k--)
+		{
+			printf(" ");
+		}
+		
+		
+		for(int j=1;j<=2*i-1;j++)
+		{
+			
+			
+		    printf("1");
+		    
+	     	
+		}
+		    printf("\n");   
+    	
+		
+		
+}
+}
